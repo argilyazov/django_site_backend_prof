c54b11b4f7d38f612459ab9c0303381ece84ddb4
argilyazov <arsel.giliazov@urfu.me>
2023-01-11 23:48:30 +0500

    Создан нормальный шаблон сайта с хэдером сайдбаром и футером, и добавлена навигация между страницами

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('',views.index )
+    path('',views.index ),
+    path('demand',views.demand ),
+    path('geography',views.geography ),
+    path('skills',views.skills ),
+    path('last_vac',views.last_vac )
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,20 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'main/index.html')
+
+
+def demand(request):
+    return render(request,'main/demand.html')
+
+
+def geography(request):
+    return render(request,'main/geography.html')
+
+
+def skills(request):
+    return render(request,'main/skills.html')
+
+
+def last_vac(request):
+    return render(request,'main/last_vac.html')
+
